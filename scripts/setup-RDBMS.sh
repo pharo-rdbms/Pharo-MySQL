8ad653fcc8be899163b29c677be4d113ef6abf48
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2020-07-14 16:10:58 -0300

    Adapt mysql scripts

--- scripts/setup-RDBMS.sh
+++ scripts/setup-RDBMS.sh
@@ -10,11 +10,13 @@ if  [[ "$RDBMS" = MariaDB* ]]; then
     mariadb/server:$DOCKER_IMAGE_VERSION
 elif  [[ "$RDBMS" = MySQL* ]]; then
     readonly DOCKER_IMAGE_VERSION=$(echo $RDBMS | cut --complement -c -6)
-    docker run -d -p 127.0.0.1:3306:3306 \
-      -e MYSQL_ROOT_PASSWORD=sodbxtest \
+    docker run --name=rdbms-server -d -p 127.0.0.1:3306:3306 \
+      -e MYSQL_ROOT_PASSWORD=secret \
       -e MYSQL_ROOT_HOST=% \
       -e MYSQL_DATABASE=sodbxtest2 \
-      -e MYSQL_USER=sodbxtest \
-      -e MYSQL_PASSWORD=sodbxtest \
       mysql/mysql-server:$DOCKER_IMAGE_VERSION
+    docker exec -it rdbms-server mysql --user=root --password=secret --execute "CREATE DATABASE IF NOT EXISTS sodbxtest2;" --verbose
+    docker exec -it rdbms-server mysql --user=root --password=secret --execute "CREATE USER sodbxtest@localhost IDENTIFIED BY 'sodbxtest';" --verbose
+    docker exec -it rdbms-server mysql --user=root --password=secret --execute "GRANT ALL PRIVILEGES ON *.* TO 'sodbxtest'@'localhost';" --verbose
+    docker exec -it rdbms-server mysql --user=root --password=secret --execute "FLUSH PRIVILEGES;" --verbose
 fi;

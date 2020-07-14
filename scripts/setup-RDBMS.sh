842c3ccf52cb9c83dd11c4b6e358b0083338b5ff
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2020-07-14 14:51:32 -0300

    Update setup script

--- scripts/setup-RDBMS.sh
+++ scripts/setup-RDBMS.sh
@@ -3,14 +3,14 @@
 if  [[ "$RDBMS" = MariaDB* ]]; then
   readonly DOCKER_IMAGE_VERSION=$(echo $RDBMS | cut --complement -c -8)
   docker run -d -p 127.0.0.1:3306:3306 \
-    -e MYSQL_ROOT_PASSWORD=secret \
-    -e MYSQL_DATABASE=test \
+    -e MYSQL_ROOT_PASSWORD=sodbxtest \
+    -e MYSQL_DATABASE=sodbxtest2 \
     mariadb/server:$DOCKER_IMAGE_VERSION
 elif  [[ "$RDBMS" = MySQL* ]]; then
     readonly DOCKER_IMAGE_VERSION=$(echo $RDBMS | cut --complement -c -6)
     docker run -d -p 127.0.0.1:3306:3306 \
-      -e MYSQL_ROOT_PASSWORD=secret \
+      -e MYSQL_ROOT_PASSWORD=sodbxtest \
       -e MYSQL_ROOT_HOST=% \
-      -e MYSQL_DATABASE=test \
+      -e MYSQL_DATABASE=sodbxtest2 \
       mysql/mysql-server:$DOCKER_IMAGE_VERSION
 fi;
